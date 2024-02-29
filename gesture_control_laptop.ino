48b9262a48d384635deed4e74b1710b37802b966
Aditya Gurnani <2018.aditya.gurnani@ves.ac.in>
2024-02-29 09:33:27 -0500

    Update gesture_control_laptop.ino

--- gesture_control_laptop.ino
+++ gesture_control_laptop.ino
@@ -1,11 +1,22 @@
+#include <avr/sleep.h>
+
+#include <avr/power.h>
+
 const int Trigger_R = 2;
 const int Echo_R = 3;
 const int Trigger_L = 4;
 const int Echo_L = 5;
 const int LED_Pin = 13;
+const int WakeUp_Pin = 2;
 
 float minDistR = 50.0;
 float minDistL = 50.0;
+
+void wakeUpNow() {
+  Serial.println("Waking up, starting recalibration...");
+  calibrateSensors();
+  Serial.println("Recalibration completed.");
+}
 
 void setup() {
   Serial.begin(9600);
@@ -14,6 +25,7 @@ void setup() {
   pinMode(Trigger_L, OUTPUT);
   pinMode(Echo_L, INPUT);
   pinMode(LED_Pin, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(2), wakeUpNow, LOW); // Assuming digital pin 2 is used for wake-up
 
   Serial.println("Calibration Start");
   calibrateSensors();
@@ -43,6 +55,15 @@ float Distance(int Trigger, int Echo) {
   return min(distance, 50);
 }
 
+void goToSleep() {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Deepest sleep mode
+  sleep_enable(); // Enable sleep mode
+  sleep_mode(); // Enter sleep mode
+
+  // Device is now asleep and will continue from here when woken up
+  sleep_disable(); // Disable sleep mode after wake up
+}
+
 void loop() {
 
   float distance1 = Distance(Trigger_R, Echo_R);
@@ -50,14 +71,14 @@ void loop() {
 
   if (distanceR > 0 && distanceR < 10) {
     Serial.println("Volume_Up");
-    digitalWrite(LED_Pin, HIGH);
+    digitalWrite(feedbackLED, HIGH);
     delay(500);
-    digitalWrite(LED_Pin, LOW);
+    digitalWrite(feedbackLED, LOW);
   } else if (distanceL > 0 && distanceL < 10) {
     Serial.println("Volume_Down");
-    digitalWrite(LED_Pin, HIGH); 
+    digitalWrite(feedbackLED, HIGH); // Feedback
     delay(500);
-    digitalWrite(LED_Pin, LOW);
+    digitalWrite(feedbackLED, LOW);
   }
 
   while (distance1 < 20 && distance1 > 0) {
@@ -66,25 +87,19 @@ void loop() {
     distance2 = Distance(Trigger_L, Echo_L);
     if (distance1 == 50) {
       Serial.println("Forward");
-      digitalWrite(LED_Pin, HIGH); 
       delay(1000);
-      digitalWrite(LED_Pin, LOW);
     }
     distance1 = Distance(Trigger_R, Echo_R);
     distance2 = Distance(Trigger_L, Echo_L);
     if (distance1 > 0 && distance1 < 20 && distance2 == 50) {
       Serial.println("Scroll_Up");
-      digitalWrite(LED_Pin, HIGH); 
       delay(1);
-      digitalWrite(LED_Pin, LOW);
     }
   }
 
   if (distance1 > 25 && distance1 < 50 && distance2 == 50) {
     Serial.println("Scroll_Down");
-    digitalWrite(LED_Pin, HIGH); 
     delay(100);
-    digitalWrite(LED_Pin, LOW);
   }
 
   while (distance2 < 20 && distance2 > 0) {
@@ -93,33 +108,44 @@ void loop() {
     distance2 = Distance(Trigger_L, Echo_L);
     if (distance2 == 50) {
       Serial.println("Rewind");
-      digitalWrite(LED_Pin, HIGH); 
       delay(1000);
-      digitalWrite(LED_Pin, LOW);
     }
 
     distance1 = Distance(Trigger_R, Echo_R);
     distance2 = Distance(Trigger_L, Echo_L);
     if (distance2 > 0 && distance2 < 20 && distance1 == 50) {
       Serial.println("Change_Tab");
-      digitalWrite(LED_Pin, HIGH); 
       delay(1000);
-      digitalWrite(LED_Pin, LOW);
     }
   }
 
   if (distance2 > 25 && distance2 < 50 && distance1 == 50) {
     Serial.println("Drop_Down");
-    digitalWrite(LED_Pin, HIGH); 
     delay(1000);
-    digitalWrite(LED_Pin, LOW);
   }
   if ((distance1 > 0 && distance1 < 50) && (distance2 > 0 && distance2 < 50)) {
     Serial.println("Enter");
-    digitalWrite(LED_Pin, HIGH); 
     delay(1500);
-    digitalWrite(LED_Pin, LOW);
   }
+  if (distanceR < 10 && distanceL > 30 && distanceL < 50) {
+    Serial.println("Volume_Up");
+    delay(500); // Short delay to prevent multiple triggers
+  }
+  // Volume Down: Left hand moves closer while right hand remains static
+  else if (distanceL < 10 && distanceR > 30 && distanceR < 50) {
+    Serial.println("Volume_Down");
+    delay(500);
+  }
+  // Existing gestures for reference
+  else if (distanceR < 20 && distanceR > 0) {
+    Serial.println("Scroll_Up");
+    delay(300);
+  } else if (distanceL < 20 && distanceL > 0) {
+    Serial.println("Change_Tab");
+    delay(300);
+  }
+
   delay(100);
+  goToSleep();
 
 }
